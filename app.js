3e74159872456b5e329df477a64f1ba2211abb77
thebarbakov <94574341+thebarbakov@users.noreply.github.com>
2022-07-17 18:31:35 +0300

    project 15 upd

--- app.js
+++ app.js
@@ -12,25 +12,64 @@ const NotFound = require('./errors/NotFound');
 const { login, createUser } = require('./controllers/users');
 
 const { userAuth } = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-// mongoose.connect(
-//   'mongodb+srv://admin:admin@mesto.rhsuxz1.mongodb.net/?retryWrites=true&w=majority',
-//   {
-//     useNewUrlParser: true,
-//   },
-// );
+mongoose.connect(
+  'mongodb+srv://admin:admin@mesto.rhsuxz1.mongodb.net/?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+  },
+);
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-});
+// mongoose.connect('mongodb://localhost:27017/mestodb', {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//     autoIndex: true,
+// });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+const allowedCors = [
+  'localhost:3000',
+  'http://localhost:3000',
+  'http://127.0.0.1:3000',
+  /(https|http)?:\/\/(?:www\.|(?!www))mesto.online.nomoredomains.work\/[a-z]+\/|[a-z]+\/|[a-z]+(\/|)/,
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+
+  if (allowedCors.some((e) => e.test && e.test(origin)) || allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  return next();
+});
+
+app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.post(
   '/signin',
@@ -64,6 +103,8 @@ app.use('/cards', require('./routes/cards'));
 app.all('*', (req, res, next) => {
   next(new NotFound('Неправильный путь'));
 });
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use(errorHandler);
